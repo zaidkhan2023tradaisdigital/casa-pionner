90021fddbd4f8e09f4505f5fd5ee0f47ea416044
zaidkhan <zaidkhan@tradiasdigital.com>
2023-10-09 15:11:39 +0530

    add model remove plan section and about us text

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     dots:false,
+//     // navText:["<span class='bi bi-arrow-left'></span> <small class='dif'>East 1530 sq ft</small>",    "<small class='dif2'>North 1520 sq ft</small> <span class='bi bi-arrow-right'></span> "],
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+// function validation(){
+//     var text = document.getElementById("text1").value
+//     var email = document.getElementById("email").value
+//     var text2 = document.getElementById("text2").value
+//     var message = document.getElementById("message").value
+
+//     if(text1 == ""){
+//         return false;
+//     }
+    
+//     else{
+//         document.write("<h1 id='cong'>Congratulation Message Has been Send<h1>")
+//     }
+// }
+
+
+let btnDownload = document.querySelector(".btn-download")
+let form = document.querySelector(".model-item")
+let closeBtn = document.querySelector(".close")
+
+
+
+btnDownload.addEventListener("click", ()=>{
+    form.classList.add("active")
+   
+
+})
+
+
+closeBtn.addEventListener("click" , ()=> {
+    form.classList.remove("active")
+})
+
+
